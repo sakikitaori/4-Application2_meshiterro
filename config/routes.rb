421fe08bb55ffaba7b6a031d7c8477d7507e8a35
sakikitaori <98959152+sakikitaori@users.noreply.github.com>
2022-02-22 03:59:03 +0900

    [Finish]4-24

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+
+
+  get 'homes/about' => 'homes#about', as: 'about'
+
+
+
+
+  # get 'homes/top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
